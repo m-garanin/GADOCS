78b9fc6a349b4af5e2e55a2c91f35c2168ce5e21
Michael Garanin <m-garanin@yandex.ru>
2020-05-09 14:40:55 +0300

    sp

--- build/build.py
+++ build/build.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import os
+from shutil import copyfile
+
+import markdown2
 
 G_PATH = "../CONTENT"
 G_OUT = "../OUT"
@@ -8,7 +11,7 @@ def start():
     
     for root, dirs, files in os.walk(G_PATH):
         convert_one_folder(root, files)
-        continue
+        
 
     
 def convert_one_folder(root, files):
@@ -16,11 +19,13 @@ def convert_one_folder(root, files):
     if not os.path.exists(en_md):
         return
 
+    path = root.split(os.sep)
+    # path[2:]
     for fn in files:
-        print root, fn
-        #convert_one_file(root, fn)
+        #print root, fn
+        convert_one_file(path[2:], fn)
         
-            
+           
 def convert_one_file(paths, fname):
     folder = os.path.join(G_OUT, *paths)
     if not os.path.exists(folder):
@@ -28,12 +33,36 @@ def convert_one_file(paths, fname):
 
     if fname.endswith(('jpeg', 'png', 'jpg')):
         return convert_image(paths, fname)
+
+    if fname.endswith(".md"):
+        return convert_md(paths, fname)
         
-    print folder, fname
+    #print folder, fname
+
+    
+def convert_md(paths, fname):
+    md_path = os.path.join(G_PATH, *paths) + "/" + fname
+
+    if fname == "en.md":
+        lang_fname = "index.html"
+    else:
+        lang_fname = fname.replace(".md", ".html")
+    
+    html_dst = os.path.join(G_OUT, *paths) + "/" +  lang_fname
+    
+    html = markdown2.markdown_path(md_path)
+    #print html
+    f = open(html_dst, "wb")
+    f.write(html.encode('utf-8'))
+    f.close()
+    
+    
 
 def convert_image(paths, fname):
-    print "IMAGE:", fname
-    
+    src = os.path.join(G_PATH, *paths) + "/" + fname
+    dst = os.path.join(G_OUT, *paths) + "/" +  fname
+    #print src, dst
+    copyfile(src, dst)
 
 if __name__ == '__main__':
     #with app.app_context():
