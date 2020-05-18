8800fd1ad1c81ed40117f30104cbcac13e750374
Michael Garanin <m-garanin@yandex.ru>
2020-05-18 21:37:44 +0300

    sp

--- CONTENT/RTMP/rtmp-step-by-step/en.md
+++ CONTENT/RTMP/rtmp-step-by-step/en.md
@@ -1,6 +1,4 @@
-![image](q1.png)
-
-   **Step by Step**
+**Step by Step**
 ===
 It’s basic “how-to” when hosts in same local network.
 
--- build/build.py
+++ build/build.py
@@ -12,18 +12,26 @@ from sections import GROUPS
 
 G_PATH = "../CONTENT"
 G_OUT = "../OUT"
+G_POSTS = {} # {name: {'en': ('en-title', 'en-txt'), 'ru': ('ru-title','ru-txt'), ...} }
 
 app = Flask(__name__)
 
 
 def start():
-    #print GROUPS
-    for root, dirs, files in os.walk(G_PATH):
-        convert_one_folder(root, files)
-        
 
+    # STAGE 1 (prepare posts in memory)
+    for root, dirs, files in os.walk(G_PATH):
+        s1_convert_one_folder(root, files)
+
+    # STAGE 2 (render to html)
+    #print G_POSTS
+    for pname, langs in G_POSTS.items():
+        s2_render_page(pname, langs)
     
-def convert_one_folder(root, files):
+#--------------------------------------------------
+#  STAGE 1
+#--------------------------------------------------
+def s1_convert_one_folder(root, files):
     en_md = os.path.join(root, 'en.md')
     if not os.path.exists(en_md):
         return
@@ -32,52 +40,100 @@ def convert_one_folder(root, files):
     # path[2:]
     for fn in files:
         #print root, fn
-        convert_one_file(path[2:], fn)
+        s1_convert_one_file(path[2:], fn)
         
            
-def convert_one_file(paths, fname):
-    folder = os.path.join(G_OUT, *paths)
+def s1_convert_one_file(paths, fname):
+    folder = os.path.join(G_OUT, paths[-1])
     if not os.path.exists(folder):
         os.makedirs(folder)
 
     if fname.endswith(('jpeg', 'png', 'jpg')):
-        return convert_image(paths, fname)
+        return s1_convert_image(paths, fname)
 
     if fname.endswith(".md"):
-        return convert_md(paths, fname)
+        return s1_convert_md(paths, fname)
         
     #print folder, fname
 
     
-def convert_md(paths, fname):
+def s1_convert_md(paths, fname):
     md_path = os.path.join(G_PATH, *paths) + "/" + fname
 
+    post_name = paths[-1]
+    lang = fname.replace(".md","")
+    
     if fname == "en.md":
         lang_fname = "index.html"
     else:
         lang_fname = fname.replace(".md", ".html")
     
-    html_dst = os.path.join(G_OUT, *paths) + "/" +  lang_fname
-    
+    md_text = open(md_path, 'r').read()
+    title = get_title(md_text)
     content = markdown2.markdown_path(md_path)
+    add_post(post_name, lang, title, content)
     
-    data = dict(content=content)
+    
+def s1_convert_image(paths, fname):
+    src = os.path.join(G_PATH, *paths) + "/" + fname
+    dst = os.path.join(G_OUT, paths[-1]) + "/" +  fname
+    #print src, dst
+    copyfile(src, dst)
+
+    
+def get_title(txt):
+    "read title from text (as first line)"
+    parts = txt.split("\n")
+    title = ""
+    for p in parts:
+        if p:
+            title = p
+            break
+    title = title.replace("**","").replace("#", "").strip()
+    return title
+
+
+def add_post(pname, lan, title, html):
+    """
+    pname - post name (rtmp-example-post)
+    lan - language code (en, ru, es)
+    title - title by language
+    html - html content
+    """
+    p_inf = G_POSTS.get(pname, {})
+    p_inf[lan] = (title, html)
+    G_POSTS[pname] = p_inf
+
+
+#--------------------------------------------------
+#  STAGE 2
+#--------------------------------------------------
+def s2_render_page(pname, langs):
+    for lng, (title, content) in langs.items():
+        s2_render_lang(pname, lng, title, content)
+
+        
+def s2_render_lang(pname, lng, title, content):
+    print lng, title
+
+    if lng == "en":
+        lang_fname = "index.html"
+    else:
+        lang_fname = lng + ".html"
+    
+    html_dst = os.path.join(G_OUT, pname) + "/" +  lang_fname
+
+    data = dict(title=title, content=content)
     html = render_page('post.html', data)
     
     #print html
     f = open(html_dst, "wb")
     f.write(html.encode('utf-8'))
     f.close()
+
+
     
     
-
-def convert_image(paths, fname):
-    src = os.path.join(G_PATH, *paths) + "/" + fname
-    dst = os.path.join(G_OUT, *paths) + "/" +  fname
-    #print src, dst
-    copyfile(src, dst)
-
-
 def render_page(tmpl_name, pg):
     ctx = {'time': int(time())}
     
